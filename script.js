25e4b64522f18bb424e01b21565103aa983ef802
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-08-13 14:40:09 +0100

    refactored render function

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
 let myLeads = [];
-
+let oldLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
-
-/* 
-localStorage.clear()
-leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage)       
-*/
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-deleteBtn.addEventListener('dblclick', function() {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-
-})
-
-inputBtn.addEventListener('click', function() {
-  myLeads.push(inputEl.value);
-  renderLeads();
-  inputEl.value = '';
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads()
-})
-
-
-
-function renderLeads() {
+function render(leads) {
   let listItems = '';
-  for (let i = 0; i < myLeads.length; i++) {
- // listItems += '<li><a href="' +  myLeads[i] + '" target="_blank">' + myLeads[i] + '</a></li>';
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+        <a href="${leads[i]}" target="_blank">${leads[i]}</a>
       </li>`;
     
  }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener('dblclick', function() {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+
+})
+
+inputBtn.addEventListener('click', function() {
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads)
+})
